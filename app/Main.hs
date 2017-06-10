b45ad12ef71e10eb18b21491021e97af12d45406
binesiyu <yubinexsaber@gmail.com>
2017-06-10 17:03:16 +0800

    init repo

---
+++ app/Main.hs
@@ -0,0 +1,36 @@
+module Main where
+
+import           Data.List
+
+solveRPN :: (Num a,Read a) => String -> a
+{- solveRPN expression = head (foldl foldingFunction [] (words expression)) -}
+    {- where foldingFunction stack item = error "test" -}
+
+solveRPN  = head . foldl foldingFunction [] . words
+    where foldingFunction (x:y:ys) "*"    = (x*y):ys
+          foldingFunction (x:y:ys) "+"    = (x+y):ys
+          foldingFunction (x:y:ys) "-"    = (y-x):ys
+          foldingFunction xs numberString = read numberString:xs
+
+testRPN1 = solveRPN "10 4 3 + 2 * -"
+
+solveRPN' :: String -> Float
+solveRPN' = head . foldl foldingFunction [] . words
+    where foldingFunction (x:y:ys) "*"    = (x*y):ys
+          foldingFunction (x:y:ys) "+"    = (x+y):ys
+          foldingFunction (x:y:ys) "-"    = (y-x):ys
+          foldingFunction (x:y:ys) "/"    = (y/x):ys
+          foldingFunction (x:y:ys) "^"    = (y ** x):ys
+          foldingFunction (x:xs) "ln"     = log x:xs
+          foldingFunction xs "sum"        = [sum xs]
+          foldingFunction xs numberString = read numberString:xs
+
+testRPNFload = solveRPN' "2.7 ln"
+
+
+main :: IO ()
+{- main = someFunc -}
+main = print "helloworld"
+
+a = "test"
+
